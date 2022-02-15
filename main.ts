d6c9bbc918bce2863ff29bb28eb0824ae5737293
fujisaki <>
2022-02-15 13:19:53 +0900

    fix: 不要部分を削除

--- main.ts
+++ main.ts
@@ -33,61 +33,30 @@ const coordinates = [
   [138.247243, 38.317245],
 ];
 
-//東京ディズニーリゾートを取り囲む矩形
-const disneyCoordinates = [
-  [139.86605288747617, 35.63996853858424],
-  [139.86604639720952, 35.61776367832307],
-  [139.90449404938747, 35.61769049507307],
-  [139.90446611619558, 35.639954583440804],
-  [139.86605288747617, 35.63996853858424],
-];
-
 //佐渡島を囲む多角形
 const polyFeature = new Feature({
   geometry: new Polygon([coordinates]),
 });
 
-//ディズニーを囲む多角形
-const disneyFeature = new Feature({
-  geometry: new Polygon([disneyCoordinates]),
-});
-
 //経度と緯度をwebメルカルトへ変換
 polyFeature.getGeometry().transform("EPSG:4326", "EPSG:3857");
-disneyFeature.getGeometry().transform("EPSG:4326", "EPSG:3857");
 
 //見た目を定義
-const styles = new Style({
+const SadogaShimaStyles = new Style({
   fill: new Fill({
-    // color: "rgba(0,5,255,0.3)",
+    color: "rgba(0,5,255,0.3)",
   }),
 });
 
 const vectorLayer = new VectorLayer({
   source: new VectorSource({
-    features: [polyFeature, disneyFeature],
+    features: [polyFeature],
   }),
-  style: styles,
+  style: SadogaShimaStyles,
 });
 
 // 画像の範囲  (座標の配列で、[left(左下の座標の経度), bottom(左下の座標の緯度), right(右上の座標の経度), top(右上の座標の緯度)]の順になっている。）
 // fromLonLat()で経度緯度をwebメルカルトに変換。
-const imageExtent: number[] = fromLonLat([
-  139.86604639720952, 35.61776367832307,
-]).concat(fromLonLat([139.90446611619558, 35.639954583440804]));
-
-const imageLayer = new Image({
-  source: new ImageStatic({
-    url: disneyImage,
-    projection: new Projection({
-      code: "xkcd-image",
-      units: "pixels",
-    }),
-    imageExtent: imageExtent,
-  }),
-  className: "disney",
-});
-
 //作ったwebメルカルト地図
 var mapExtent = fromLonLat([139.865957, 35.617521]).concat(
   fromLonLat([139.904628, 35.64006])
@@ -113,6 +82,7 @@ const map = new Map({
       source: new OSM(),
     }),
     layer,
+    vectorLayer,
   ],
   view: new View({
     center: fromLonLat([139.86605288747617, 35.63996853858424]),
